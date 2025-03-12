b8861bc3f54561114f84f62d4cde0d957250edf4
Ygorhenrique <118612433+YgorHenriqueLima@users.noreply.github.com>
2025-03-12 09:11:59 -0300

    adicionando exercícios de aulas de c++

---
+++ aula5/declaracoes_multiplas_var.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+/*CONSTANTE*/
+#define pi 3.1415;
+#define canal "canal Fessor Bruno \n";
+
+int main(){
+    /*
+    int vidas;
+    int tiros;
+    int quant_munição;
+    */
+
+    /*int vidas;int tiros;int quant_municao;*/
+
+    int vidas=200, poder=300, quant_municao=200;
+ 
+    cout << vidas << "\n";
+    cout << poder << "\n";
+    cout << quant_municao << "\n";
+    cout << pi;
+    cout << canal;
+    
+    return 0;
+}
---
+++ aula6/variaveis_globais_locais.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int n1,n2; // variáveis globais
+
+int main(){
+
+    int n3, n4; //variáveis locais
+
+    return 0;
+}
